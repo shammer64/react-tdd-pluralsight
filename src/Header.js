4d3d8204197e19a692fd56f0f0be7f8cc817ab6e
Scott Hammer <scott.a.hammer@outlook.com>
2022-03-03 13:36:14 -0500

    SH Created React App, created Header component and first test

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-Intro">This is the header!</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
